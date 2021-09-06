6606d4a2bf1130177114f9e41dac58bf297c1992
Anna Ayvazyan <anayvazyan95@gmail.com>
2021-09-07 00:24:49 +0400

    anuliky avelacrela slice kruma massivy u heta talis arajin tviic sksac minchev erkrord tivy(voch neraryal)

--- script.js
+++ script.js
@@ -55,15 +55,55 @@
 // }
 //
 // console.log(bigest([3, 11, 4, -2]))
+//
+// function zuyger(numbers) {
+//     const x = []
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] % 2 === 0) {
+//             x.push(numbers[i])
+//         }
+//     }
+//     return x
+// }
+//
+// console.log(zuyger([2, 4, 5, 7, 8, 9]))
 
-function zuyger(numbers) {
-    const x = []
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 2 === 0) {
-            x.push(numbers[i])
-        }
-    }
-    return x
-}
+// function accum(s) {
+//     let result = []
+//     for (let i = 0; i < s.length; i++) {
+//         let arrayItem = new Array(i + 1).fill(s[i])
+//         arrayItem[0] = arrayItem[0].toUpperCase()
+//         result.push(arrayItem.join(''))
+//     }
+//     return result.join('-')
+// }
+// console.log(accum('absd'));
+//
+// function highAndLow(numbers) {
+//  return Math.max(numbers)
+// }
+// console.log(highAndLow(1,2,5,6));
+//
+// polindrome('anna');
+// function polindrome(str){
+// return str===str.split('').reverse().join('')
+// }  console.log (str)
 
-console.log(zuyger([2, 4, 5, 7, 8, 9]))
+// let arr = [ 'pixik', 'shunik', 'pisik']
+// let x=arr.shift();
+// console.log(x)
+
+// function zzvanq () {
+//     return 155
+// }
+// let x = zzvanq()
+// console.log(x)
+// let arr = [ 'pixik', 'shunik', 'pisik']
+// arr.push('hafo');
+// console.log(arr)
+// let arr = [ 'pixik', 'shunik', 'pisik']
+// let karobchka =arr.pop();
+// console.log(karobchka)
+let arr = [ 'pixik', 'shunik', 'pisik', 'hafo']
+let x= arr.slice(1,3);
+console.log(x)
